eaf1cd1fcebb42b3136b822ad83521fc3bc71d36
Syed Shehzaan Nadeem <nsyedshehzaan@gmail.com>
2026-03-11 21:00:37 +0530

    Added Online Learning Platform Project

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ function playVideo(videoId){
 
 let player = document.getElementById("videoPlayer");
 
-player.src = "https://www.youtube.com/embed/" + videoId;
+player.src = "https://www.youtube.com/embed/" + videoId + "?autoplay=1";
 
 }
+
+
+
 
 
 function checkQuiz(){
@@ -24,4 +27,30 @@ localStorage.setItem("quizScore", score);
 
 }
 
+
+function loginUser(){
+
+let username = document.getElementById("username").value;
+let password = document.getElementById("password").value;
+let message = document.getElementById("loginMessage");
+
+if(username === "student" && password === "1234"){
+
+message.style.color = "green";
+message.innerText = "Login Successful!";
+
+setTimeout(function(){
+window.location.href = "course.html";
+},1500);
+
+}
+
+else{
+
+message.style.color = "red";
+message.innerText = "Invalid Username or Password";
+
+}
+
+}
 
