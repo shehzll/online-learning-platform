3912b1b50061b9f33d0670a7932bce5834ed2cb1
Syed Shehzaan Nadeem <nsyedshehzaan@gmail.com>
2026-03-11 20:02:00 +0530

    Online Learning Platform - CodTech Internship Task 2

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-function startLearning(){
-
-alert("Welcome to LearnHub! Choose a course to start learning.");
-
-}
-
 function playVideo(videoId){
 
 let player = document.getElementById("videoPlayer");
@@ -12,19 +6,22 @@ player.src = "https://www.youtube.com/embed/" + videoId;
 
 }
 
+
 function checkQuiz(){
 
 let score = 0;
 
 let q1 = document.querySelector('input[name="q1"]:checked');
 let q2 = document.querySelector('input[name="q2"]:checked');
-let q3 = document.querySelector('input[name="q3"]:checked');
 
 if(q1) score += parseInt(q1.value);
 if(q2) score += parseInt(q2.value);
-if(q3) score += parseInt(q3.value);
 
 document.getElementById("result").innerText =
-"Your Score: " + score + " / 3";
+"Your Score: " + score;
+
+localStorage.setItem("quizScore", score);
 
 }
+
+
